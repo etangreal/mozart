d939fb66bf3de59070b9344bf5b748ffeda3531e
Tersius Kuhne <ktersius@gmail.com>
2021-05-28 07:48:59 +1200

    play around with nx/ts configs

--- apps/infrastructure/src/main.ts
+++ apps/infrastructure/src/main.ts
@@ -1,10 +1,9 @@
-#!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { SrcStack } from '../lib/src-stack';
 
-const app = new cdk.App();
-new SrcStack(app, 'SrcStack', {
+const main = new cdk.App();
+new SrcStack(main, 'SrcStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
