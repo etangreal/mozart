32f2d3b57e176f7ad3071f2c61c96c21685e88b8
Tersius Kuhne <ktersius@gmail.com>
2021-06-07 08:31:17 +1200

    Change commands to npx

--- apps/infra/lib/cdk-pipelines-stack.ts
+++ apps/infra/lib/cdk-pipelines-stack.ts
@@ -33,14 +33,14 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
                 sourceArtifact,
                 cloudAssemblyArtifact,
                 installCommand: [
-                    'npm install -g nx',
+                   // 'npm install -g nx',
                     'yarn install --frozen-lockfile'
                 ].join(' && '),
                 // We need a build step to compile the TypeScript Lambda
                 buildCommand: [
-                    'nx run infra:build'
+                    'npx nx run infra:build'
                 ].join(' && '),
-                synthCommand: 'cdk synth -a dist/apps/infra/main.js'
+                synthCommand: 'npx cdk synth -a dist/apps/infra/main.js'
             }),
         });
 
